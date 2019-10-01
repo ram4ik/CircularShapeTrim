30e892a39a2be1b155c8b9c9f7dca1af28c0eb56
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-01 12:18:09 +0300

    init

--- CircularShapeTrim/ContentView.swift
+++ CircularShapeTrim/ContentView.swift
@@ -9,8 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var circularProgress: CGFloat = 1.0
+    private var circularPercentage: Int { Int(circularProgress * 100.0)}
     var body: some View {
-        Text("Hello World")
+        VStack(spacing: 40) {
+            Text("Circular Shapes").font(.largeTitle)
+            Text("Trim Function").font(.title).foregroundColor(.gray)
+            Text("Trim your shape to only show a percentage of it.")
+            
+            Circle()
+                .trim(from: 0, to: circularProgress)
+                .stroke(Color.purple, style: StrokeStyle(lineWidth: 40, lineCap: CGLineCap.round))
+                .frame(height: 300)
+                .rotationEffect(.degrees(-90))
+                .overlay(Text("\(circularPercentage)%")
+                .font(.largeTitle)
+                .foregroundColor(.gray)
+                .padding(40))
+            
+            VStack {
+                Text("Progress")
+                HStack {
+                    Text("0%")
+                    Slider(value: $circularProgress)
+                    Text("!00%")
+                }
+            }.padding()
+        }
     }
 }
 
